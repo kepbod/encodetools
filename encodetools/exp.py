c142aa1d2fc48db9e6a12f1678c1d25a61724ffd
Xiao-Ou Zhang <kepbod@gmail.com>
2020-08-27 22:47:55 -0400

    modify

--- encodetools/exp.py
+++ encodetools/exp.py
@@ -14,7 +14,7 @@ class Exp(Entry):
     '''
 
     def __init__(self, exp, parse=True):
-        if not exp.startswith('ENCSR'): # in case exp is not an experiment ID
+        if not exp.startswith('ENCSR'):  # in case exp is not an experiment ID
             raise Exception('{} is not one experiment ID!'.format(exp))
         entry_type = 'experiments'
         super(Exp, self).__init__(exp, entry_type)
@@ -29,21 +29,17 @@ class Exp(Entry):
         self.description = self._json['description']
         # biosample info
         self.biosample = self._json['biosample_summary']
-        self.biosample_type = self._json['biosample_type']
-        self.biosample_id = self._json['biosample_term_id']
         # target
         if 'target' in self._json:
             self.target = self._json['target']['label']
             self._attr.update({'target': 'Target'})
         # update available attributes
         self._attr.update({'description': 'Description',
-                           'biosample': 'Biosample',
-                           'biosample_type': 'Biosample Type',
-                           'biosample_id': 'Biosample ID'})
+                           'biosample': 'Biosample'})
 
     def fetch_control(self):
-        controls = self._json['possible_controls'] 
+        controls = self._json['possible_controls']
         if controls:
-            return [ctrl['accession'] for ctrl in controls] 
+            return [ctrl['accession'] for ctrl in controls]
         else:
             return None
